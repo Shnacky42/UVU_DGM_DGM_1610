9849bab69c969208b2a9804958e3ee8ccf437b9a
Mark <10856722@my.uvu.edu>
2020-09-23 12:11:02 -0600

    Plane Project Done

---
+++ Plane Project/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private float speed = 15.0f;
+    private float rotationSpeed = 45.0f;
+    private float verticalInput;
+    private float horizontalInput;
+
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        // Tryingsomething out here
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * horizontalInput * Time.deltaTime);
+
+       
+    }
+}
+//Examples of the car
+//transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+//transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * verticalInput);
